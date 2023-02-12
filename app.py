68017b7658494a2783af5ddd5c052c900784794c
turekradek <turekradek@gmail.com>
2023-02-12 14:16:32 +0100

    jsons

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import os
+import pandas as pd
+import datetime as dt
+import jsons
+import json
 from flask import Flask , render_template, url_for, request, redirect
 # import csv
 
@@ -12,7 +16,13 @@ def hello_world():
 
 @app.route("/restapi")
 def restapi():
+    all_jsons = pd.read_json(r'all_folders.json' )
+    all_jsons_html = all_jsons.to_html()
+    now = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return f"""
-            Tu będzie strona do pobierania jsonow
+            Tu będzie strona do pobierania jsonow <br>
+            {now}
+            {all_jsons_html}
+            
 """
 
