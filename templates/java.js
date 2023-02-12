22ddf8d0f1870d7761d7d871e374ea961dd366b6
turekradek <turekradek@gmail.com>
2023-02-12 11:30:51 +0100

    templates

---
+++ templates/java.js
@@ -0,0 +1,15 @@
+function zegar()
+{
+    var dzisiaj = new Date();
+    var dzien = dzisiaj.getDate();
+    var miesiac = dzisiaj.getMonth() + 1;
+    var rok = dzisiaj.getFullYear();
+    var gdziina = dzisiaj.getHours();
+    if (godzina < 10 ) godzina = "0" + godzina;
+    var minuta = dzisiaj.getMinutes();
+    if (minuta < 10 ) minuta = "0" + minuta;
+    var sekunda = dzisiaj.getSeconds();
+    if (sekunda < 10 ) sekunda = "0" + sekunda;
+    document.getElementById("czas").innerHTML = dzien + "." + miesiac + "." + rok + "     "+ godzina + ":"+minuta+":"+sekunda
+    setTimeout("zegar()",1000)
+}
